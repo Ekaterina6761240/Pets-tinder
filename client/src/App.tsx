c15a2366160fddc62e70adac19dcf866e391c5be
Vladislav <naveless@list.ru>
2023-06-29 18:07:45 +0300

    Sidebar done

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,9 @@
 import React, { useState } from 'react';
 import './App.css';
+import SideBar from './components/ui/SideBar';
 
 function App(): JSX.Element {
-  return <div className="App">Привет!</div>;
+  return <SideBar />;
 }
 
 export default App;
